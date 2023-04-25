e200194da876bc0a1ea8aed82a88a4377c211052
Xo4yxa <serj.kolodin2014@yandex.ru>
2023-04-25 22:45:22 +0300

    sql

---
+++ Product.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Апр 25 2023 г., 22:35
+-- Версия сервера: 5.6.51
+-- Версия PHP: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `Product`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `Products`
+--
+
+CREATE TABLE `Products` (
+  `ID` int(255) NOT NULL,
+  `PRODUCT_ID` int(255) NOT NULL,
+  `PRODUCT_NAME` varchar(255) NOT NULL,
+  `PRODUCT_PRICE` int(255) NOT NULL,
+  `PRODUCT_ARTICLE` int(255) NOT NULL,
+  `PRODUCT_QUANTITY` int(255) NOT NULL,
+  `DATE_CREATE` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `Products`
+--
+ALTER TABLE `Products`
+  ADD PRIMARY KEY (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
